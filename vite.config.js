33730e3af46573ef808d0beb618fb4f28f784047
Harshith <147683599+SriHarshith27@users.noreply.github.com>
2024-04-27 03:42:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'VITE_REACT_APP_API_URL=https://myproject-2200090170.onrender.com',
+        target: 'https://myproject-2200090170.onrender.com',
         changeOrigin: true,
         secure: false,    
         rewrite: (path) => path.replace(/^\/api/, ''),
